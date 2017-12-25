1a00615568b0b09ec588966175750a2a5145e854
Mask <770284144@qq.com>
2017-12-25 22:03:21 +0800

    Add files via upload

---
+++ base.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <cstring>
+
+#define ERROR -1
+#define OK 1
+#define OVERFLOW -1
+typedef bool Status;
+using namespace std;
